4cb0d3e6b3aa6888a0bcafbac724ad7d956a97e4
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-08-15 13:33:48 -0300

    correção em casa

--- 15-08-23.java
+++ 15-08-23.java
@@ -11,7 +11,7 @@ public class Main
 		
 		while (sair == 0) {
 		    
-		System.out.printf("\n\t\tMENU");
+		System.out.printf("\n\t\tMENU\n");
 		System.out.printf("\n\t1 - Soma");
 		System.out.printf("\n\t2 - Subtração");
 		System.out.printf("\n\t3 - Divisão");
@@ -25,24 +25,28 @@ public class Main
 		        valor1 = scan.nextInt();
 		        valor2 = scan.nextInt();
 		        resultado = valor1 + valor2;
+		        System.out.printf("\nO resultado da soma é %d\n", resultado);
 		        break;
 		    case 2:
 		        System.out.println("\nDigite dois valores para subtrai-los: \n");
 		        valor1 = scan.nextInt();
 		        valor2 = scan.nextInt();
 		        resultado = valor1 - valor2;
+		        System.out.printf("\nO resultado da subtração é %d\n", resultado);
 		        break;
 		    case 3: 
 		        System.out.println("\nDigite dois valores para dividi-los: \n");
-		        valor1 = scan.nextInt();
-		        valor2 = scan.nextInt();
-		        resultado = valor1 / valor2;
+		        double Dvalor1 = scan.nextDouble();
+		        double Dvalor2 = scan.nextDouble();
+		        double resultadod = Dvalor1 / Dvalor2;
+		        System.out.printf("\nO resultado da divisão é %.2f\n", resultadod);
 		        break;
 		    case 4:
 		        System.out.println("\nDigite dois valores para multiplica-los: \n");
 		        valor1 = scan.nextInt();
 		        valor2 = scan.nextInt();
 		        resultado = valor1 * valor2;
+		        System.out.printf("\nO resultado da multiplicação é %d\n", resultado);
 		        break;
 		    case 0:
 		        sair = 1;
@@ -51,7 +55,7 @@ public class Main
 		        System.out.println("Opção inválida.");
 		        break;
 		}
-		System.out.printf("\nO resultado é %d\n", resultado);
+		
 	    }
 	}
 }
