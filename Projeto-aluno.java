892e97f7e9406d0b05dfb56d96a7197a325a1147
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-09-07 10:59:06 -0300

    Update Projeto-aluno.java

--- Projeto-aluno.java
+++ Projeto-aluno.java
@@ -66,13 +66,89 @@ public class Main
 	            default:
 	                break;
 	        }
+	        
 	    }
-
+	    System.out.println("\tDados Atualizados:");
+        System.out.printf("\nMatrícula: %s\nNome: %s\nCurso: %s\nTurma: %s\nPeriodo: %d\n", c1.getMatricula(), c1.getNome(), c1.getCurso(), c1.getTurma(), c1.getPeriodo());
 	    
 	    
-	    System.out.printf("\nMatrícula: %s\nNome: %s\nCurso: %s\nTurma: %s\nPeriodo: %d", c1.getMatricula(), c1.getNome(), c1.getCurso(), c1.getTurma(), c1.getPeriodo());
+	    //ALUNO 2
+	    System.out.printf("\nDeseja inserir os dados de outro aluno? 1- SIM, 2- NÃO\n");
+	    int novoAluno = scan.nextInt();
+	    
+	    if(novoAluno == 1) {
+	    Aluno c2 = new Aluno();
+		
+		System.out.println("Digite o nome do aluno: ");
+		String nome2 = scan.nextLine();
+		c2.setNome(nome2);
+		
+		System.out.println("Digite o curso do aluno: ");
+		String curso2 = scan.nextLine();
+		c2.setCurso(curso2);
+		
+		System.out.println("Digite a turma do aluno (A, B, C ou D): ");
+		String turma2 = scan.nextLine();
+		c2.setTurma(turma2);
+		
+		System.out.println("Digite o período do aluno (1 - 8): ");
+		int periodo2 = scan.nextInt();
+		c2.setPeriodo(periodo2);
+		
+		System.out.printf("\nMatrícula: %s\nNome: %s\nCurso: %s\nTurma: %s\nPeriodo: %d", c2.getMatricula(), c2.getNome(), c2.getCurso(), c2.getTurma(), c2.getPeriodo());
+	    
+	    System.out.println("\n\nDigite sua nota do 1 Bimestre: ");
+	    double nota12 = scan.nextDouble();
+	    System.out.println("\nDigite sua nota do 2 Bimestre: ");
+	    double nota22 = scan.nextDouble();
+	    
+	    String status2;
+	    
+	    if (nota12+nota22 < 6) {
+	        status2 = "Reprovado.";
+	    } else if(nota12+nota22 < 14) {
+	        System.out.println("\nDigite sua nota da prova final");
+	        double notaFinal2 = scan.nextDouble();
+	        if (notaFinal2 + ((nota12+nota22)/2) < 10) {
+	            status2 = "Reprovado.";
+	        } else {
+	            status2 = "Aprovado.";
+	        } 
+	    }else {
+	        status2 = "Aprovado.";
+	    }
+	    
+	    System.out.println("\n" + status2);
+	    
+	    if (status2 == "Aprovado.") {
+	        c2.passarPeriodo();
+	        System.out.println("\n\nO aluno deseja ir para qual turma para o próximo período? \n1-A\n2-B\n3-C\n4-D");
+	        int novaTurma2 = scan.nextInt();
+	        switch (novaTurma2){
+	            case 1:
+	                c2.setTurma("A");
+	                break;
+	            case 2:
+	                c2.setTurma("B");
+	                break;
+	            case 3:
+	                c2.setTurma("C");
+	                break;
+	            case 4:
+	                c2.setTurma("D");
+	            default:
+	                break;
+	        }
+	        
+	    }
+	    System.out.println("\tDados Atualizados:");
+        System.out.printf("\nMatrícula: %s\nNome: %s\nCurso: %s\nTurma: %s\nPeriodo: %d\n", c2.getMatricula(), c2.getNome(), c2.getCurso(), c2.getTurma(), c2.getPeriodo());
+	    
+	    }
+	    System.out.println("\nQuantidade total de alunos: "+Aluno.qtdAlunos);
 	}
 }
+
 
 
 
@@ -84,11 +160,14 @@ public class Aluno
     //MATRICULA
     private String matricula;
     
+    public static int qtdAlunos = 0;
+    
     public Aluno() {
         Random rand = new Random ();
         int i = rand.nextInt(10000);
         String matricula = "2023" + i; 
         this.matricula = matricula;
+        qtdAlunos++;
     }
     
     public String getMatricula () {
