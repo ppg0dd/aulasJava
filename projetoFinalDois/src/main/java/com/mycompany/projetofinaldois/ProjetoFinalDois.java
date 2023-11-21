863a0b2762a7dda009aea32fde1cf03ed6f60d54
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-11-21 10:22:26 -0300

    Update ProjetoFinalDois.java

--- projetoFinalDois/src/main/java/com/mycompany/projetofinaldois/ProjetoFinalDois.java
+++ projetoFinalDois/src/main/java/com/mycompany/projetofinaldois/ProjetoFinalDois.java
@@ -3,9 +3,18 @@ package com.mycompany.projetofinaldois;
 public class ProjetoFinalDois {
 
     public static void main(String[] args) {
-        Cliente c = new Cliente ("vasco", "123");
-        c.comprar(1234, 1, 1200);
-        c.comprar(1235, 1, 2200);
-        c.imprimirCompras();
+        Cliente c1 = new Cliente ("Pedro", "123");
+        c1.comprar(1, 1200);
+        c1.comprar(1, 2200);
+        c1.comprar(2, 3000);
+        c1.imprimirCompras();
+        c1.valorGasto();
+        
+        Cliente c2 = new Cliente ("João", "122");
+        c2.comprar(3, 500);
+        c2.comprar(2, 800);
+        c2.comprar(3, 400);
+        c2.imprimirCompras();
+        c2.valorGasto();
     }
 }
